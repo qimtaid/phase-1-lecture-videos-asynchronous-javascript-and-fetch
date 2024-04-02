1138bd7b823df129cfb364f50bec559c60ab44b5
Dan Koskei <qimtaid@gmail.com>
2024-04-02 17:16:14 +0300

    add

---
+++ index.js
@@ -0,0 +1,23 @@
+console.log('before')
+setTimeout(()=> console.log('during'), 3000)
+console.log('after')
+
+console.log('before fetch')
+fetch('http://localhost:3000')
+
+
+
+
+
+
+
+
+
+// console.log('one')
+// console.log('two')
+// function foo(){
+//     SVGComponentTransferFunctionElement.log('three')
+// }
+
+// foo()
+// document.querySelector('button').addEventListener
